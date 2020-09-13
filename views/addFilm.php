2dabdbaa7041fc278c48b7010541372919b3c214
Pau Sanz <Pau@192.168.1.136>
2020-09-13 12:58:31 +0200

    - Edit view film added succesfully.

--- views/addFilm.php
+++ views/addFilm.php
@@ -1,8 +1,8 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6 offset-md-3 text-center">
-            <h3>Film added successfully!</h3>
-            <a href="?ctl=show&act=films">Show Films</a>       
+            <h3 class="text-center">Film added succesfully!</h3>
+            <a href="?ctl=show&act=films"><button id="returnHome" class="btn btn-secondary">Show films</button></a>
         </div>         
     </div>
 </div>
